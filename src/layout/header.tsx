396be0318c4f325fa718108d17f54686b2247a7d
Leonardo Muros do Nascimento <lmuros@gmail.com>
2020-08-20 00:09:08 -0300

    'atualizacao'

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -5,11 +5,7 @@ function Header(){
         <div className="header">
             <div className="brand">            
                 Loja Pokémon Fire       
-            </div>
-            {/*Implementação futura
-            <div className="header-links">
-                <a href="cart.html">Carrinho</a>
-            </div>  */}          
+            </div>                  
         </div>     
     )
 }
